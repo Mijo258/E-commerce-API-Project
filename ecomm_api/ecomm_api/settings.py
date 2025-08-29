21586d4e5c349330cbd1ffb694538da9e3bfed98
mohammed <mijogiddo@gmail.com>
2025-08-30 00:47:35 +0300

    Configure project to use MySQL database

--- ecomm_api/ecomm_api/settings.py
+++ ecomm_api/ecomm_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ecomm_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecomm_api_db',
+        'USER': 'ecomm_user',
+        'PASSWORD': 'a_strong_password', # Use the password from step 1
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
