6096e73a4bfd16c7ab036294772c57540f78fe0f
mohammed <mijogiddo@gmail.com>
2025-08-30 19:58:37 +0300

    Implement filtering, search, and pagination

--- ecomm_api/ecomm_api/settings.py
+++ ecomm_api/ecomm_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'products', # this is the product app 
     'rest_framework',
     'users', # this is the users app
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -130,8 +131,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+# In ecomm_api/settings.py
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),  
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
--- ecomm_api/products/views.py
+++ ecomm_api/products/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters import rest_framework as filters
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
+
+class ProductFilter(filters.FilterSet):
+    # Allow filtering by price range 
+    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte') # gte = greater than or equal to
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte') # lte = less than or equal to
+    # Allow searching by name 
+    name = filters.CharFilter(field_name="name", lookup_expr='icontains')
+
+    class Meta:
+        model = Product
+        # Define exact match filters here
+        fields = ['category', 'name', 'min_price', 'max_price']
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -18,3 +31,5 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('-created_at')
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filterset_class = ProductFilter
+    search_fields = ['name', 'category__name']
