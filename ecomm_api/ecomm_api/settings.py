0fa2f62d339210ae6db6090c2fa351349bac49c7
mohammed <mijogiddo@gmail.com>
2025-08-30 15:19:46 +0300

    in this phase i added and configured the api and added an authentiation methods for users to add or modify products list, and make the category list available for any one to see

--- ecomm_api/ecomm_api/settings.py
+++ ecomm_api/ecomm_api/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products', # this is the product app 
+    'rest_framework',
+    'users', # this is the users app
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- ecomm_api/products/views.py
+++ ecomm_api/products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    A simple ViewSet for viewing categories.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing products.
+    """
+    queryset = Product.objects.all().order_by('-created_at')
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
