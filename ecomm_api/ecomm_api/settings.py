0eeff893b46dedb9abbde58171d830452a89a779
mohammed <mijogiddo@gmail.com>
2025-09-08 01:36:37 +0300

    fix python anywhere setup

--- ecomm_api/ecomm_api/settings.py
+++ ecomm_api/ecomm_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bxrps@5rs0i2qt$hcb#x0x6o#ubl(cb3rkdfh!enqb)b*)2x!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mohamedalmajzoub.pythonanywhere.com']
 
 
 # Application definition
