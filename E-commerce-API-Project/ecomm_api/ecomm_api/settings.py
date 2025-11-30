5c0e67fcd06dae7a91866e0f9bc78b14775a7fdb
Mohamedalmajzoub Osman Saeed <mijogiddo@gmail.com>
2025-12-01 01:40:54 +0200

    some security modifications

--- E-commerce-API-Project/ecomm_api/ecomm_api/settings.py
+++ E-commerce-API-Project/ecomm_api/ecomm_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bxrps@5rs0i2qt$hcb#x0x6o#ubl(cb3rkdfh!enqb)b*)2x!6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['mohamedalmajzoub.pythonanywhere.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
